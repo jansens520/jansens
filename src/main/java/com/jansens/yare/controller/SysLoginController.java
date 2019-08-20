dab2331eb35a57734d765c1eae1b1e6f1711cb21
Administrator <2251478696@qq.com>
2019-08-20 10:18:04 +0800

    更新

--- src/main/java/com/jansens/yare/controller/SysLoginController.java
+++ src/main/java/com/jansens/yare/controller/SysLoginController.java
@@ -3,8 +3,18 @@ package com.jansens.yare.controller;
 import com.google.code.kaptcha.Constants;
 import com.google.code.kaptcha.Producer;
 import com.jansens.yare.common.utils.IOUtils;
+import com.jansens.yare.core.http.HttpResult;
+import com.jansens.yare.model.SysUser;
+import com.jansens.yare.security.JwtAuthenticatioToken;
+import com.jansens.yare.service.SysUserService;
+import com.jansens.yare.util.PasswordUtils;
+import com.jansens.yare.util.SecurityUtils;
+import com.jansens.yare.vo.LoginBean;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.imageio.ImageIO;
@@ -20,6 +30,10 @@ public class SysLoginController {
 
     @Autowired
     private Producer producer;
+    @Autowired
+    private SysUserService sysUserService;
+    @Autowired
+    private AuthenticationManager authenticationManager;
 
     @GetMapping("captcha.jpg")
     public void captcha(HttpServletResponse response, HttpServletRequest request) throws ServletException, IOException {
@@ -36,5 +50,39 @@ public class SysLoginController {
         ServletOutputStream out = response.getOutputStream();
         ImageIO.write(image, "jpg", out);
         IOUtils.closeQuietly(out);
+    }
+
+    /**
+     * 登录接口
+     */
+    @PostMapping(value = "/login")
+    public HttpResult login(@RequestBody LoginBean loginBean, HttpServletRequest request) throws IOException {
+        String username = loginBean.getAccount();
+        String password = loginBean.getPassword();
+        String captcha = loginBean.getCaptcha();
+        // 从session中获取之前保存的验证码跟前台传来的验证码进行匹配
+        Object kaptcha = request.getSession().getAttribute(Constants.KAPTCHA_SESSION_KEY);
+        if(kaptcha == null){
+            return HttpResult.error("验证码已失效");
+        }
+        if(!captcha.equals(kaptcha)){
+            return HttpResult.error("验证码不正确");
+        }
+        // 用户信息
+        SysUser user = sysUserService.findByName(username);
+        // 账号不存在、密码错误
+        if (user == null) {
+            return HttpResult.error("账号不存在");
+        }
+        if (!PasswordUtils.matches(user.getSalt(), password, user.getPassword())) {
+            return HttpResult.error("密码不正确");
+        }
+        // 账号锁定
+        if (user.getStatus() == 0) {
+            return HttpResult.error("账号已被锁定,请联系管理员");
+        }
+        // 系统登录认证
+        JwtAuthenticatioToken token = SecurityUtils.login(request, username, password, authenticationManager);
+        return HttpResult.ok(token);
     }
 }
